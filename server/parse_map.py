807ef188ff57bb4ea989b8f76f1601371685e2fb
Kevin Chou <kchou@tinyco.com>
2014-01-24 22:55:23 -0800

    udp stuffs

--- server/parse_map.py
+++ server/parse_map.py
@@ -12,6 +12,13 @@ def generate_grid(width, height):
     column = ['0' for y in range(height)]
     return [column[:] for x in range(width)]
 
+def print_grid(grid, width, height):
+    for y in range(height):
+        row = []
+        for x in range(width):
+            row.append(str(grid[x][y]).zfill(3))
+        print ' '.join(row)
+
 map_data = open_map('sample.json')
 
 width, height = map_data['width'], map_data['height']
@@ -23,9 +30,11 @@ grid = generate_grid(width, height)
 map_data_index = 0
 for y in range(height):
     for x in range(width):
-        grid[x][y] = data[map_data_index]
+        grid[x][y] = map_data['layers'][0]['data'][map_data_index]
         map_data_index += 1
 
-for x in range(width):
-    for y in range(height):
-        grid[x][y]
+print_grid(grid, width, height)
+
+# for x in range(width):
+#     for y in range(height):
+#         grid[x][y]
--- server/server.py
+++ server/server.py
@@ -18,7 +18,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "10.1.7.109", 1234
 
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
