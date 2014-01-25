bdc37b32a18d23dcd2faa3db78a5cb8dff851cdc
Kevin Chou <kchou@tinyco.com>
2014-01-24 22:29:32 -0800

    stuff

--- server/parse_map.py
+++ server/parse_map.py
@@ -14,9 +14,18 @@ def generate_grid(width, height):
 
 map_data = open_map('sample.json')
 
-width = map_data['width']
-height = map_data['height']
+width, height = map_data['width'], map_data['height']
 
 print 'Dimensions {} x {}'.format(width, height)
 
 grid = generate_grid(width, height)
+
+map_data_index = 0
+for y in range(height):
+    for x in range(width):
+        grid[x][y] = data[map_data_index]
+        map_data_index += 1
+
+for x in range(width):
+    for y in range(height):
+        grid[x][y]
